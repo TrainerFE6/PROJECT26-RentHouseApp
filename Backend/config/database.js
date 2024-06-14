8591653522610e533b5cf98a06e978ea08418ed4
TegarRobiBakhtiar <149132728+TegarRobiBakhtiar@users.noreply.github.com>
2024-06-14 18:04:48 +0700

    Delete Backend directory

--- Backend/config/database.js
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from "sequelize";
-
-const db = new Sequelize('Ckeoeq2LSZGLA110', 'WfAjHThDptCwP8FA', 'VxqvThhECCDRVavr', {
-    host: 'educalab.id',
-    port: 5000,
-    dialect: 'mysql'
-});
-
-export default db;
